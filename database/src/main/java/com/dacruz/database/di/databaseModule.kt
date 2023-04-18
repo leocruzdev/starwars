907d497d6261b9c4d74419dde0cf855ed8aa4dc9
Leonardo Cruz <leonardo_cruz_git@outlook.com>
2023-04-18 01:09:12 -0300

    test(character): testing module character, database creation

---
+++ database/src/main/java/com/dacruz/database/di/databaseModule.kt
@@ -0,0 +1,11 @@
+package com.dacruz.database.di
+
+import com.dacruz.database.AppDatabase
+import com.dacruz.database.AppDatabase.Companion
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+val databaseModule = module {
+    single { Companion.getDatabase(androidApplication()) }
+    single { get<AppDatabase>().favoriteCharacterDao() }
+}
